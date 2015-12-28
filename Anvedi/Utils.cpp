feccf017cb53b7c994795f09acd88d7c667d7457
ilpropheta <marcoarena@email.it>
2015-12-28 17:16:21 +0100

    increasing color close threshold

--- Anvedi/Utils.cpp
+++ Anvedi/Utils.cpp
@@ -7,7 +7,7 @@ bool close(const QColor& c1, const QColor& c2)
 	return std::sqrt(
 		std::norm(c1.red() - c2.red()) +
 		std::norm(c1.green() - c2.green()) +
-		std::norm(c1.blue() - c2.blue())) < 10;
+		std::norm(c1.blue() - c2.blue())) < 50;
 }
 
 QVector<qreal> ToVector(const QVariant& var)
